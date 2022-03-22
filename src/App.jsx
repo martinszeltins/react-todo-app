ad404f1e5ae0f325203e18abcf087c8d4113cc96
Martins <martins.zeltins@arkbauer.com>
2022-03-22 17:22:46 +0200

    Refactoring and cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,16 @@ import { useState } from 'react'
 function App() {
     const [count, setCount] = useState(0)
 
+    const increment = () => {
+        setCount(count => {
+            return count + 1
+        })
+    }
+
     return (
         <button
             className="bg-blue-400 text-white rounded px-4 py-2 m-2"
-            onClick={() => setCount((count) => count + 1)}>
+            onClick={increment}>
 
             count is: { count }
         </button>
