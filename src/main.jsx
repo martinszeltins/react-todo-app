18ac9d0d5a9fd322584159d08789698389129253
Martins <martins.zeltins@arkbauer.com>
2022-03-23 14:40:23 +0200

    Cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './assets/css/app.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('app'))
